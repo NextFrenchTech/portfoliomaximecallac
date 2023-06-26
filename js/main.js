034d6629ec59339b3023c9373350ddd309571f0d
NextFrenchTech <ikiboo.sys@gmail.com>
2023-06-26 19:49:49 +0200

    new view

--- js/main.js
+++ js/main.js
@@ -1,20 +1,46 @@
 /*automatic title toogle inside*/
 
-/*let alertshow = false;
+/*let titleMessage = false;
+const title = "Welcome in my Laboratory";
+const message = "Enjoy it !";
+const changeTitle = setInterval(() => {
+    document.title = 
+    titleMessage    ? title
+                    : message ;
+    titleMessage = !titleMessage;
+}, 1667);*/
+
+/*let titleMessage = false;
 setInterval(() => {
     document.title = 
-    alertshow   ? documentTitle
+    titleMessage   ? documentTitle
                 : "Enjoy it !";
-    alertshow = !alertshow;
-}, 1000);*/
+    titleMessage = !titleMessage;
+}, 1667);*/
+
+/*let titleMessage = ["Welcome in my Laboratory", "Enjoy it !", ];
+
+let counter = 0;
+
+setInterval(function() {
+    document.title = titleMessage [counter % titleMessage.length]; counter++
+}, 1667);*/
 
 /*title toogle outside*/
 
 let documentTitle = document.title; 
-window.addEventListener("blur", () => {
-    document.title = "OH, No Way... Come Back HERE !"; 
-});
 
 window.addEventListener("focus", () => {
     document.title = documentTitle;
 });
+
+window.addEventListener("blur", () => {
+    document.title = "OH, No Way... Come Back HERE !"; 
+});
+
+/*window.addEventListener("focus", function() {
+    this.document.title = "Welcome in my Laboratory";
+});
+window.addEventListener("blur", function() {
+    this.document.title = "OH, No Way... Come Back HERE !"; 
+});*/
