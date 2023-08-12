4459e212a0076de8452a83606b9c36c01dda3714
NextFrenchTech <ikiboo.sys@gmail.com>
2023-08-12 19:07:03 +0200

    patch view

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,38 +1,24 @@
 /*random chars text*/
 
-const effectElement = document.querySelector(".effect")
+const effectElement = document.querySelector(".effect");
 
-effectElement.addEventListener("mouseover", applyEffect)
+effectElement.addEventListener("mouseover", applyEffect);
 
 function applyEffect(e) {
-	const alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	let i = 0
+	const alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	let i = 0;
     
 	const interval = setInterval(() => {
         effectElement.innerText = effectElement.innerText
             .split("")
             .map((c, idx) => {
-                if (idx < i) return e.target.dataset.value[idx]
-                return alphabets[Math.floor(Math.random() * 26)]
+                if (idx < i) return e.target.dataset.value[idx];
+                return alphabets[Math.floor(Math.random() * 26)];
             })
-            .join("")
+            .join("");
 		
-		if(i>=e.target.dataset.value.length) clearInterval(interval)
-		i += 1/5
+		if(i>=e.target.dataset.value.length) clearInterval(interval);
+		i += 1/5;
 		
-	}, 75)
-}
-
-/*overlapping chars with shadow*/
-
-const overlapEls = document.querySelectorAll(".overlap") || [];
-overlapEls.forEach(el => {
-    const chars = [...el.textContent];
-    el.innerHTML = "";
-    chars.forEach((char,index) => {
-        const span = document.createElement("span");
-        span.textContent = char;
-        span.style.setProperty("--index", index);
-        el.append(span);
-    });
-});
+	}, 75);
+};
--- js/main.js
+++ js/main.js
@@ -96,3 +96,18 @@ let urlAnimate = ()=> {
     setTimeout(urlAnimate, 70);
 };
 urlAnimate();
+
+
+/*overlapping chars with shadow*/
+
+const overlapEls = document.querySelectorAll(".overlap") || [];
+overlapEls.forEach(el => {
+    const chars = [...el.textContent];
+    el.innerHTML = "";
+    chars.forEach((char,index) => {
+        const span = document.createElement("span");
+        span.textContent = char;
+        span.style.setProperty("--index", index);
+        el.append(span);
+    });
+});
